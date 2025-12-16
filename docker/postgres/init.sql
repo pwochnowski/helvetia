9ab85e08bd207f512f164098e1a85fc80f325226
Paul Wochnowski <paulw@Pauls-MacBook-Pro.local>
2025-12-16 10:22:32 +0800

    Delete psql remnants

--- docker/postgres/init.sql
+++
@@ -1,9 +0,0 @@
--- Maybe just generate this script inside `build_images.sh`
-
--- COPY logical/*.sql /docker-entrypoint-initdb.d/
--- COPY init.sql /docker-entrypoint-initdb.d/
--- \i /docker-entrypoint-initdb.d/logical/user.sql;
--- \i /docker-entrypoint-initdb.d/logical/article.sql;
--- \i /docker-entrypoint-initdb.d/logical/read.sql;
--- \i /docker-entrypoint-initdb.d/logical/be_read.sql;
--- \i /docker-entrypoint-initdb.d/logical/popular_rank.sql;
--- docker/postgres/logical/popular_rank.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE popular_rank (
-    id TEXT PRIMARY KEY,                 -- e.g. "pr42"
-    timestamp BIGINT NOT NULL,
-    temporalGranularity TEXT CHECK (
-        temporalGranularity IN ('daily', 'weekly', 'monthly')
-    ),
-    articleAidList TEXT                  -- comma-separated list of article AIDs
-);
